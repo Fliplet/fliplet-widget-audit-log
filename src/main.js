50074d5c86b59739072f08662b29b65301102fe1
Tony Wu <tonytlwu@users.noreply.github.com>
2022-07-04 12:16:58 +0100

    Initialize Vue instance after Fliplet() is resolved

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import AuditLog from './AuditLog';
 
-new Vue({
-  el: '#audit-log',
-  render: (createElement) => {
-    return createElement(AuditLog);
-  }
+Fliplet().then(() => {
+  new Vue({
+    el: '#audit-log',
+    render: (createElement) => {
+      return createElement(AuditLog);
+    }
+  });
 });
